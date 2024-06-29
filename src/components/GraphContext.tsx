7c3f7963153ce0fb65eb2f10c3a03f2ec134fd09
mracsys <mracsys@gmail.com>
2024-06-29 03:36:07 -0400

    change deploy action from github pages to remote host

--- src/components/GraphContext.tsx
+++ src/components/GraphContext.tsx
@@ -1,6 +1,6 @@
 import { GraphPlugin, WorldGraphFactory } from "@mracsys/randomizer-graph-tool";
 import { createContext } from "react";
 
-const defaultGraph = WorldGraphFactory('ootr', {}, '7.1.143', {files: {}});
+const defaultGraph = WorldGraphFactory('ootr', {}, '7.1.143', {files: {}, subfolder: ''});
 
 export const GraphContext = createContext<GraphPlugin>(defaultGraph);
