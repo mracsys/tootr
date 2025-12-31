8c30a46b7bc8e7a092e8dcdf84b56c282304d7e6
mracsys <mracsys@gmail.com>
2025-12-31 14:58:59 -0500

    ootr 9.0 stable

--- src/components/Tracker.tsx
+++ src/components/Tracker.tsx
@@ -108,7 +108,7 @@ const Tracker = (_props: {}) => {
 
     // user settings panel
     const [savedSettingsVersion, setSavedSettingsVersion] = useState<number>(1);
-    const [graphVersion, setGraphVersion] = useState<string>('8.3.69 Dev');
+    const [graphVersion, setGraphVersion] = useState<string>('9.0.0 Release');
     const [playerNumber, setPlayerNumber] = useState<number>(0);
     const [settingIcons, setSettingIcons] = useState<boolean>(true);
     const [regionPage, setRegionPage] = useState<string>('Overworld');
